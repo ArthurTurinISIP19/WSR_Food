0fd07f8aeca8005400191fc302b19b111d30092a
Turin Arthur <ezeka1n@mail.ru>
2022-05-13 10:51:29 +0900

    ><

--- app/src/main/java/com/example/food_wsr/MainActivity.java
+++ app/src/main/java/com/example/food_wsr/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.food_wsr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.test.espresso.web.sugar.Web;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, OnBoardingScreen1.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, 2000);
     }
 }
